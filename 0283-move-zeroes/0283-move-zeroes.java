402f2cb14a04c51f51b65fe27f6afc97a654fab0
tubaglacs25-glitch <tuba.gla_cs25@gla.ac.in>
2026-09-24 00:59:40 +0530

    Time: 2 ms (91.69%), Space: 47.7 MB (75.86%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,13 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i=0;
-        for(int j=0;j<nums.length;j++){
-            if(nums[j]!=0){
-                int temp=nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
-                i++;
-            }
+      int i=0;
+      for(int j=0;j<nums.length;j++){
+        if(nums[j]!=0){
+            int temp=nums[j];
+            nums[j]=nums[i];
+            nums[i]=temp;
+            i++;
         }
+      }
     }
 }
